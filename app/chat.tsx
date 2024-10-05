21a9283cc374f7219ffe03d691277c8eee17508d
lukas-schmid <schmid.luk@hotmail.com>
2024-10-05 14:26:25 +0200

    move footer in own component

--- app/chat.tsx
+++ app/chat.tsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import {
   View,
-  Text,
-  TextInput,
-  ActivityIndicator,
   StyleSheet,
   KeyboardAvoidingView,
   Platform,
@@ -11,7 +8,6 @@ import {
 } from "react-native";
 import {
   collection,
-  addDoc,
   query,
   orderBy,
   limit,
@@ -21,25 +17,20 @@ import {
   QueryDocumentSnapshot,
   DocumentData,
 } from "firebase/firestore";
-import { useLocalSearchParams } from "expo-router";
 import { db } from "../firebaseConfig";
-import { Button } from "@/components/Button";
 import { Message } from "@/types/message";
 import { MessageContainer } from "@/components/MessageContainer";
 import { Colors } from "@/constants/Colors";
+import { Footer } from "@/components/Footer";
 
 const MESSAGE_LIMIT = 25;
 
 const ChatScreen = () => {
-  const flatListRef = useRef<FlatList>(null);
-  const { displayName } = useLocalSearchParams();
+  const flatListRef = useRef<FlatList<Message>>(null);
   const [messages, setMessages] = useState<Message[]>([]);
-  const [newMessage, setNewMessage] = useState("");
   const [lastVisibleMessage, setLastVisibleMessage] =
     useState<QueryDocumentSnapshot<DocumentData> | null>(null);
-  const [sendingMessage, setSendingMessage] = useState(false);
 
-  // Fetch initial messages and set up listener for real-time updates
   useEffect(() => {
     const messagesRef = collection(db, "lukas-global-chat-app");
     const q = query(
@@ -66,7 +57,6 @@ const ChatScreen = () => {
     return () => subscribe();
   }, []);
 
-  // fetch next 10 messages
   const fetchOlderMessages = () => {
     const messagesRef = collection(db, "lukas-global-chat-app");
     const next = query(
@@ -93,28 +83,6 @@ const ChatScreen = () => {
     });
   };
 
-  // Send a message
-  const handleSendMessage = async () => {
-    if (newMessage.trim() && !sendingMessage) {
-      setSendingMessage(true);
-      const messagesRef = collection(db, "lukas-global-chat-app");
-
-      try {
-        await addDoc(messagesRef, {
-          displayName,
-          message: newMessage,
-          timestamp: new Date(),
-        });
-        setNewMessage("");
-        flatListRef.current?.scrollToIndex({ index: 0, animated: true });
-      } catch (error) {
-        console.error("Error sending message: ", error);
-      } finally {
-        setSendingMessage(false);
-      }
-    }
-  };
-
   return (
     <View style={styles.container}>
       <KeyboardAvoidingView
@@ -131,25 +99,7 @@ const ChatScreen = () => {
           inverted
           style={styles.messagesList}
         />
-
-        <View style={styles.footer}>
-          <TextInput
-            style={styles.input}
-            value={newMessage}
-            placeholder="Type a message..."
-            onChangeText={setNewMessage}
-            multiline={true}
-            blurOnSubmit={false}
-            editable={!sendingMessage}
-          />
-          <Button
-            onPress={handleSendMessage}
-            disabled={sendingMessage}
-            style={styles.customButton}
-          >
-            {sendingMessage ? <ActivityIndicator color="white" /> : "Send"}
-          </Button>
-        </View>
+        <Footer flatListRef={flatListRef} />
       </KeyboardAvoidingView>
     </View>
   );
@@ -162,31 +112,6 @@ const styles = StyleSheet.create({
   },
   messagesList: {
     paddingHorizontal: 15,
-  },
-  footer: {
-    flexDirection: "row",
-    alignItems: "center",
-    padding: 8,
-    borderTopWidth: 1,
-    borderTopColor: Colors.grayLight,
-    backgroundColor: Colors.white,
-  },
-  input: {
-    flex: 1,
-    height: 40,
-    maxHeight: 120,
-    fontSize: 18,
-    borderColor: Colors.grayLight,
-    borderWidth: 1,
-    borderRadius: 20,
-    paddingHorizontal: 16,
-    paddingVertical: 8,
-    marginRight: 8,
-    backgroundColor: Colors.white,
-  },
-  customButton: {
-    borderRadius: 20,
-    minWidth: 80,
   },
 });
 
---
+++ components/Footer.tsx
@@ -0,0 +1,94 @@
+import {
+  ActivityIndicator,
+  TextInput,
+  View,
+  StyleSheet,
+  FlatList,
+} from "react-native";
+import { Button } from "./Button";
+import { Colors } from "@/constants/Colors";
+import { RefObject, useState } from "react";
+import { useLocalSearchParams } from "expo-router";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "@/firebaseConfig";
+import { Message } from "@/types/message";
+
+interface FooterProps {
+  flatListRef: RefObject<FlatList<Message>>;
+}
+
+export const Footer = ({ flatListRef }: FooterProps) => {
+  const { displayName } = useLocalSearchParams();
+  const [newMessage, setNewMessage] = useState("");
+  const [sendingMessage, setSendingMessage] = useState(false);
+
+  const handleSendMessage = async () => {
+    if (newMessage.trim() && !sendingMessage) {
+      setSendingMessage(true);
+      const messagesRef = collection(db, "lukas-global-chat-app");
+
+      try {
+        await addDoc(messagesRef, {
+          displayName,
+          message: newMessage,
+          timestamp: new Date(),
+        });
+        setNewMessage("");
+        flatListRef.current?.scrollToIndex({ index: 0, animated: true });
+      } catch (error) {
+        console.error("Error sending message: ", error);
+      } finally {
+        setSendingMessage(false);
+      }
+    }
+  };
+
+  return (
+    <View style={styles.footer}>
+      <TextInput
+        style={styles.input}
+        value={newMessage}
+        placeholder="Type a message..."
+        onChangeText={setNewMessage}
+        multiline={true}
+        blurOnSubmit={false}
+        editable={!sendingMessage}
+      />
+      <Button
+        onPress={handleSendMessage}
+        disabled={sendingMessage}
+        style={styles.customButton}
+      >
+        {sendingMessage ? <ActivityIndicator color={Colors.white} /> : "Send"}
+      </Button>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  footer: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 8,
+    borderTopWidth: 1,
+    borderTopColor: Colors.grayLight,
+    backgroundColor: Colors.white,
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    maxHeight: 120,
+    fontSize: 18,
+    borderColor: Colors.grayLight,
+    borderWidth: 1,
+    borderRadius: 20,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    marginRight: 8,
+    backgroundColor: Colors.white,
+  },
+  customButton: {
+    borderRadius: 20,
+    minWidth: 80,
+  },
+});
