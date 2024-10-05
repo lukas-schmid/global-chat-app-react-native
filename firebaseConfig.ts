cb104f13758fae413afb1a8e901fff760ae363f8
lukas-schmid <schmid.luk@hotmail.com>
2024-10-05 14:59:52 +0200

    update firebase config

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,23 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCd_w02vyiyCNE522DcNd7gzCn5V4NAAgY",
-  authDomain: "chatapp-29145.firebaseapp.com",
-  projectId: "chatapp-29145",
-  storageBucket: "chatapp-29145.appspot.com",
-  messagingSenderId: "994388147663",
-  appId: "1:994388147663:web:1388763935798f84b626ba",
+  apiKey: "AIzaSyA1mp3N20p0xFDj2WKxaZDImNf2obbtZMs",
+  authDomain: "ekvj-chat.firebaseapp.com",
+  projectId: "ekvj-chat",
+  storageBucket: "ekvj-chat.appspot.com",
+  messagingSenderId: "162746462712",
+  appId: "1:162746462712:web:3eb657c3c319b61ea0a60d",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firestore
 const db = getFirestore(app);
 
 export { db };
